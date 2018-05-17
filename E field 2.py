7c3fb4c610a6aab1a405883de6bf009d1382e885
Yenchienhan <tp60u06@gmail.com>
2018-05-17 16:49:51 +0800

    Update E field 2.py

--- E field 2.py
+++ E field 2.py
@@ -9,17 +9,20 @@ x,y,z = np.meshgrid(x,y,z)
 fig = plt.figure()
 ax = fig.gca(projection='3d')  #畫出3維的座標軸
 
-def E(x,y,m,n,q):     
+def E(x,y,z,m,n,l,q):     
     e=8.85*10**-12           
-    EE = (1./(4*np.pi*e))*q/((x-m)**2+(y-n)**2) #Magnitude of the vector B 磁場
-    Ex=EE*(np.cos(np.arctan2(y-n,x-m)))            #By y方向磁場 arctan2(y,x)是反tan(x/y)
-    Ey=EE*(np.sin(np.arctan2(y-n,x-m)))           #Bx x方向磁場
-    Ez=0
+    EE = (1./(4*np.pi*e))*q/((x-m)**2+(y-n)**2+(z-l)**2) #Magnitude of the vector B 磁場
+    Ex=EE*(x-m)/np.sqrt((x-m)**2+(y-n)**2+(z-l)**2)            #By y方向磁場 arctan2(y,x)是反tan(x/y)
+    Ey=EE*(y-n)/np.sqrt((x-m)**2+(y-n)**2+(z-l)**2)         #Bx x方向磁場
+    Ez=EE*(z-l)/np.sqrt((x-m)**2+(y-n)**2+(z-l)**2)         #Bx x方向磁場
     return Ex,Ey,Ez
-Ex,Ey,Ez=E(x,y,0,0,2)
+Ex,Ey,Ez=E(x,y,z,5,-5,5,2)
 ax.quiver(x,y,z,Ex,Ey,Ez,color='b',length=1,normalize=True) 
-plt.title('E field of a straight wire')
+plt.title('E field')
 plt.xlabel('x')
 plt.ylabel('y')
-ax.view_init(elev=-90, azim=90)
+plt.ylabel('z')
+#ax.view_init(elev=-90, azim=90)
+
+
 plt.show()
