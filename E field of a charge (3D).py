ba958d9b9dc656af7cf0d4a3e4f866537f95860e
peishurao <38514454+peishurao@users.noreply.github.com>
2018-05-24 16:53:57 +0800

    Update E field of a charge (3D).py

--- E field of a charge (3D).py
+++ E field of a charge (3D).py
@@ -3,9 +3,9 @@
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
-x = np.linspace(-4,4,8)
-y = np.linspace(-4,4,8)
-z = np.linspace(-4,4,8)
+x = np.linspace(-4,4,6)   #以6個向量標示
+y = np.linspace(-4,4,6)
+z = np.linspace(-4,4,6)
 x,y,z = np.meshgrid(x,y,z)
 
 # 3d figure
@@ -19,13 +19,17 @@ def E(x,y,z,m,n,l,q):
     Ey=EE*(y-n)/np.sqrt((x-m)**2+(y-n)**2+(z-l)**2)         #Ey: y方向電場
     Ez=EE*(z-l)/np.sqrt((x-m)**2+(y-n)**2+(z-l)**2)         #Ez: z方向電場
     return Ex,Ey,Ez
-Ex,Ey,Ez=E(x,y,z,5,-5,5,2)
+Ex,Ey,Ez=E(x,y,z,0,0,0,2)
 ax.quiver(x,y,z,Ex,Ey,Ez,color='b',length=1,normalize=True) 
 plt.title('E field')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.ylabel('z')
+plt.plot
 #ax.view_init(elev=-90, azim=90)
+#for angle in range(0, 360):
+    #ax.view_init(30,angle)
+plt.plot( [0], [0], [0], color='red', marker='o', markersize=12)   #畫出電荷位置
 
 
 plt.show()
