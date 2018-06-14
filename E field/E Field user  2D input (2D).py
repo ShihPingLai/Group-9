75afadbf059a0ae71ee0ff6ce638be8a4bc1d989
peishurao <38514454+peishurao@users.noreply.github.com>
2018-06-14 10:27:30 +0800

    Add files via upload

---
+++ E field/E Field user  2D input (2D).py
@@ -0,0 +1,59 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(-3,4,20)
+y = np.linspace(-3,4,20)
+x,y = np.meshgrid(x,y)
+
+# 2d figure
+fig = plt.figure()
+ax = fig.add_subplot(111)  #畫出2維的座標軸
+
+def E(x,y,m,n,q):     
+    e=8.85*10**-12           
+    EE = (1./(4*np.pi*e))*q/((x-m)**2+(y-n)**2)    #Magnitude of the vector E 磁場
+    Ex=EE*(np.cos(np.arctan2(y-n,x-m)))            #Ey: y方向電場 arctan2(y,x)是反tan(x/y)
+    Ey=EE*(np.sin(np.arctan2(y-n,x-m)))            #Ex: x方向電場
+ 
+    return Ex,Ey
+
+#initialise
+ex = 0
+ey = 0
+
+n = 3 
+position = np.zeros([n, n]) # ([row, col])
+# print position
+
+for i in range(0,n):
+    for j in range(0,n):
+       position[i][j]= input("enter:")
+print position
+
+for i in range(0,n):
+    for j in range(0,n):
+        if position[i][j]==0:
+            continue          
+            
+        Ex, Ey =E(x, y, i, j, position[i][j])
+        ex = Ex + ex
+        ey = Ey + ey
+        
+        plt.plot([i], [j], color='red', marker='o', markersize=5)   
+    
+plt.quiver(x,y,ex,ey,width=0.03,units='xy',color='b', scale = 30000000000)
+        
+plt.title('E field of positive charges with user 2D input (2D)')
+plt.xlabel('x')
+plt.ylabel('y')
+#ax.view_init(elev=-90, azim=90)
+
+
+    
+plt.show()
+
---
+++ E field/E Field with user 2D input (3D).py
@@ -0,0 +1,65 @@
+
+# coding: utf-8
+
+# In[17]:
+
+
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(-1,3,6)   #以6個向量標示
+y = np.linspace(-1,3,6)
+z = np.linspace(0,4,6)
+x,y,z = np.meshgrid(x,y,z)
+
+# 3d figure
+fig = plt.figure()
+ax = fig.gca(projection='3d')  #畫出3維的座標軸
+
+def E(x,y,z,m,n,l,q):     
+    e=8.85*10**-12           
+    EE = (1./(4*np.pi*e))*q/((x-m)**2+(y-n)**2+(z-l)**2) #Magnitude of the vector  電場
+    Ex=EE*(x-m)/np.sqrt((x-m)**2+(y-n)**2+(z-l)**2)         #Ex: x方向電場 arctan2(y,x)是反tan(x/y)
+    Ey=EE*(y-n)/np.sqrt((x-m)**2+(y-n)**2+(z-l)**2)         #Ey: y方向電場
+    Ez=EE*(z-l)/np.sqrt((x-m)**2+(y-n)**2+(z-l)**2)         #Ez: z方向電場  
+    return Ex,Ey,Ez
+
+
+#initialise
+ex = 0
+ey = 0
+ez = 0
+
+n = 3 
+position = np.zeros([n, n])   # ([row, col])
+# print position
+
+for i in range(0,n):
+    for j in range(0,n):
+       position[i][j]= input("enter:")
+print position
+
+for i in range(0,n):
+    for j in range(0,n):
+        if position[i][j]==0:
+            continue          
+        plt.plot([i], [j], [0], color='red', marker='o', markersize=5)  
+        
+        Ex,Ey,Ez=E(x, y, z, i, j, 0, position[i][j])
+        ex=Ex+ex
+        ey=Ey+ey
+        ez=Ez+ez        
+
+ax.quiver(x,y,z,ex,ey,ez,color='b',length=1,normalize=True) 
+plt.title('E field of positive charges with user 2D input (3D)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.plot
+plt.show()
+
+#ax.view_init(elev=-90, azim=90)
+#for angle in range(0, 360):
+    #ax.view_init(30,angle)
+    
+plt.show()
+
